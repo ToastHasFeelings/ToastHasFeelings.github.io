76cd7643bcbc66a7fda8102d099ab87b3f8679d1
David Kou <david.kou@warriorlife.net>
2018-10-02 09:42:29 -0700

    Worked on JavaScript
    Did JavaScript chapter 2

---
+++ scripts/conversation.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+function button1() {
+	alert('OOOOOFFFFF');
+	
+}
+
+function button2() {
+	alert('EXTRA OOOOOFFFFF');
+	
+}
+
+function button3() {
+	alert('MEGA OOOOOFFFFF');
+	
+}
+
+function button4() {
+	alert('TRIPLE DOUBLE OOOOOFFFFF');
+	
+}
